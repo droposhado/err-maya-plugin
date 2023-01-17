ff6b21436387f317e8048c99194b2e575814c276
Flaverton Rodrigues Rosa <sign@droposhado.org>
2023-01-17 19:26:17 -0300

    gg

--- conftest.py
+++ conftest.py
@@ -7,6 +7,24 @@ from model import engine
 def pytest_runtest_setup(item):
     """Cleanup tables before start a new test"""
 
+    sql = text('CREATE TABLE IF NOT EXISTS
+  public.liquids (
+    id uuid NOT NULL DEFAULT uuid_generate_v4(),
+    client_name character varying NULL,
+    client_version character varying NULL,
+    creation_date timestamp without time zone NULL,
+    last_modification timestamp without time zone NULL,
+    quantity integer NULL,
+    unit character varying NULL,
+    type character varying NULL,
+    username character varying NULL
+  );
+
+ALTER TABLE
+  public.liquids
+ADD
+  CONSTRAINT liquids_pkey PRIMARY KEY (id);')
+
     sql = text('TRUNCATE liquids;')
     results = engine.execute(sql)
 
